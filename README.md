18beea8e5f8bc2bb9fe0f3a7625cc8e19e14a20d
kinnnine <nakaharaml9@gmail.com>
2026-03-04 06:31:58 +0700

    *: initial commit

---
+++ README.md
@@ -0,0 +1,86 @@
+# ocd2kki
+
+an unofficial desktop client of Yume 2kki on ynoproject.net (yume nikki online project)
+
+# screenshots
+
+<img src="docs/menu.png"></br>
+<img src="docs/game.png"></br>
+<img src="docs/settings.png"></br>
+<img src="docs/expeditions.png">
+
+# how
+
+ocd2kki is using neutralinojs, by using user's available browsers or webview for displaying "[https://ynoproject.net/2kki/](https://ynoproject.net/2kki/)" and inject a custom interface for more "native-like" experience.
+
+ocd2kki isn't hosting or stealing any ynoproject or yume 2kki developers assets or gamedata.
+
+ocd2kki is open-source at [https://github.com/kinnnine/ocd2kki](https://github.com/kinnnine/ocd2kki)
+
+# why
+
+- separate from your main browser
+- native-like experience
+- lightweight download (only 3mb)
+
+# features
+
+- window size is cropped and fit to a game screen
+- menu buttons when hovering top of game screen
+- exit from main menu works
+
+# downside
+
+- not a truly native application, it's a desktop web app duh
+
+# build
+
+requirements:
+
+- node
+- npm or pnpm
+- neutralinojs/neu
+
+install neutralinojs cli
+``
+npm install -g @neutralinojs/neu
+``
+
+build release
+``
+neu build --embed-resources
+``
+
+development run
+``
+neu run
+``
+
+# history
+
+ocd2kki was originally created on nwjs but due to stupidity cloudflare turnstile won't play nicely, thanks to neutralinojs for solving this issue by replacing nw.js entirely.
+
+### things to point out on nwjs
+
+- embedded with chromium, doesn't need user's system browser
+- iframe with nwfaketop work flawlessly, able to load any remote url
+- iframe with onload function gives you lemon
+
+### but
+
+- weird issue with useragent thingy
+- you can play just fine but unable to sign-in or even register due to cloudflare turnstile
+
+### what's good?
+
+- i don't need to rewrite the script since the way neutralinojs works, similar to iframe onload injection but use "injectScript" inside neutralino.config.json
+- friendship ended with nwjs's iframe, now neutralinojs with user's system webview is my best friend
+- you can now sign-in, hell yeah
+
+that's it.
+
+# roadmap
+
+- [ ] fix badges floating modal size
+- [ ] fix right menu buttons position
+- [ ] discord rpc
---
+++ resources/js/ocd.js
@@ -0,0 +1,151 @@
+function ocd() {
+    // greet
+    console.log('ocd2kki init!');
+
+    // get 2kki version from meta tag
+    const yno_2kki_version = document.querySelector("meta[name='2kkiVersion']").content;
+
+    // set window title
+    Neutralino.window.setTitle(`ocd2kki rel.${NL_APPVERSION} - ${yno_2kki_version} on ynoproject.net/2kki`);
+
+    // pickup gameContainer
+    const canvasContainer = document.getElementById('canvas');
+    canvasContainer.setAttribute('style', 'display: block; top: 0px; margin: none; min-width: none; width: 640px; max-width: none; min-height: none; height: 480px; max-height: none; border: 0px; image-rendering: pixelated; outline: none;');
+
+    // pickup gameChatContainer
+    const gameChatContainer = document.getElementById('gameChatContainer');
+    gameChatContainer.setAttribute('style', 'top: none; left: none; width: 640px; height: 240px; margin-top: none; bottom: 3px;');
+
+    // pickup modalContainer
+    const modalContainer = document.getElementById('modalContainer');
+    const modalFadeOutContainer = document.getElementById('modalFadeOutContainer');
+    const confirmModalContainer = document.getElementById('confirmModalContainer');
+
+    // pickup controls
+    const controls = document.getElementById('controls');
+    controls.setAttribute('style', 'z-index: 10; opacity: 1; position: fixed; top: 6px; height: 45px;')
+
+    // set hidden accountSettingsButton
+    const accountSettingsButton = document.getElementById('accountSettingsButton');
+    const loginButton = document.getElementById('loginButton');
+    const logoutButton = document.getElementById('logoutButton');
+    accountSettingsButton.setAttribute('style', 'display: none;');
+    loginButton.setAttribute('style', 'display: none;');
+    logoutButton.setAttribute('style', 'display: none;');
+
+    // pickup login and logout button into settingsModal
+    const settingsModal = document.getElementById('settingsModal');
+    const settingsModalformButtonRow = settingsModal.querySelector('.buttonRow');
+    settingsModalformButtonRow.appendChild(loginButton);
+    settingsModalformButtonRow.appendChild(logoutButton);
+
+    // workaround: set hidden fullscreen button
+    const controlsFullscreenButton = document.getElementById('controls-fullscreen')
+    controlsFullscreenButton.setAttribute('style', 'display: none;');
+
+    // fix: enable the rest of right buttons (fullscreen only buttons)
+    const fsBadgesButton = document.getElementById('fsBadgesButton');
+    const fsLocationsButton = document.getElementById('fsLocationsButton');
+    const fsCommunityScreenshotsButton = document.getElementById('fsCommunityScreenshotsButton');
+    const fsRankingsButton = document.getElementById('fsRankingsButton');
+    const fsSchedulesButton = document.getElementById('fsSchedulesButton');
+    fsBadgesButton.classList.remove('fsOnlyControl');
+    fsLocationsButton.classList.remove('fsOnlyControl');
+    fsCommunityScreenshotsButton.classList.remove('fsOnlyControl');
+    fsRankingsButton.classList.remove('fsOnlyControl');
+    fsSchedulesButton.classList.remove('fsOnlyControl');
+
+    // workaround: set hidden content element (prevent crash)
+    const mainContent = document.getElementById('content');
+    mainContent.setAttribute('style', 'display: none;');
+
+    // tweak: display controls when mouse hovering
+    const leftControls = document.getElementById('leftControls')
+    const rightControls = document.getElementById('rightControls')
+    leftControls.style.display = 'none';
+    rightControls.style.display = 'none';
+    controls.addEventListener('mousemove', function() {
+        leftControls.style.display = 'block';
+        rightControls.style.display = 'block';
+    });
+    controls.addEventListener('mouseleave', function() {
+        leftControls.style.display = 'none';
+        rightControls.style.display = 'none';
+    });
+
+    // apply ocdGameContainer element (new of original gameContainer)
+    const ocdGameContainer = document.createElement("div");
+    ocdGameContainer.id = "ocdgameContainer";
+    ocdGameContainer.appendChild(canvasContainer);
+    ocdGameContainer.appendChild(gameChatContainer);
+    ocdGameContainer.appendChild(controls);
+    document.body.appendChild(ocdGameContainer);
+
+    // apply modalContainer element
+    document.body.appendChild(modalContainer);
+    // requirements for modalContainer to work properly
+    document.body.appendChild(modalFadeOutContainer);
+    document.body.appendChild(confirmModalContainer);
+
+    // mutationobserver things
+    const config = { attributes: true, childList: true, subtree: true };
+    const callback = (mutationList, observer) => {
+      for (const mutation of mutationList) {
+            // tweak: quit when easyrpg quit from main menu by user
+            if (mutation.target.id === 'canvas' && mutation.type === 'attributes') {
+                const canvasWidth = mutation.target.getAttribute('width');
+                const canvasHeight = mutation.target.getAttribute('height');
+                if (canvasWidth === '0' && canvasHeight === '0') {
+                    Neutralino.app.exit();
+                }
+            }
+            // fix: scrollbar bug when gameChatContainer's style is changed
+            if (mutation.target.id === 'gameChatContainer' && mutation.type === 'attributes' && mutation.attributeName === 'style') {
+                observer.disconnect();
+                var gameChatContainerTemp = document.getElementById('gameChatContainer');
+                gameChatContainerTemp.setAttribute('style', 'top: none; left: none; width: 640px; height: 240px; margin-top: none; bottom: 3px;');
+                observer.observe(document.documentElement, config);
+            }
+            // fix: accountRequired buttons appearance
+            if (mutation.target.id === 'content' && mutation.type === 'attributes' && mutation.attributeName === 'class') {
+                if (mutation.target.classList.contains('loggedIn')) {
+                    var accountSettingsButtonTemp = document.getElementById('accountSettingsButton');
+                    var loginButtonTemp = document.getElementById('loginButton');
+                    var logoutButtonTemp = document.getElementById('logoutButton');
+                    var eventControlsTemp = document.getElementById('eventControls');
+                    var fsBadgesButtonTemp = document.getElementById('fsBadgesButton');
+                    accountSettingsButtonTemp.setAttribute('style', 'display: block;');
+                    loginButtonTemp.setAttribute('style', 'display: none;');
+                    logoutButtonTemp.setAttribute('style', 'display: block;');
+                    eventControlsTemp.setAttribute('style', 'display: block;');
+                    fsBadgesButtonTemp.setAttribute('style', 'display: block;');
+                } else {
+                    var accountSettingsButtonTemp = document.getElementById('accountSettingsButton');
+                    var loginButtonTemp = document.getElementById('loginButton');
+                    var logoutButtonTemp = document.getElementById('logoutButton');
+                    var eventControlsTemp = document.getElementById('eventControls');
+                    var fsBadgesButtonTemp = document.getElementById('fsBadgesButton');
+                    accountSettingsButtonTemp.setAttribute('style', 'display: none;');
+                    loginButtonTemp.setAttribute('style', 'display: block;');
+                    logoutButtonTemp.setAttribute('style', 'display: none;');
+                    eventControlsTemp.setAttribute('style', 'display: none;');
+                    fsBadgesButtonTemp.setAttribute('style', 'display: none;');
+                }
+            }
+        }
+    };
+    // observe the mutations
+    const observer = new MutationObserver(callback);
+    observer.observe(document.documentElement, config);
+};
+
+function inject() {
+    const target = document.querySelector('.loaded');
+    if (target) {
+        ocd(); 
+    } else {
+        setTimeout(inject, 100);
+    }
+};
+Neutralino.init();
+inject();
